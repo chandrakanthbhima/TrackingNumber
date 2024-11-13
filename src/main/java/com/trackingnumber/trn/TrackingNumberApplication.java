d203b917de44f333d6edef1023816a6e23957abd
Chandrakanth <chandrakanth.bhima@gmail.com>
2024-11-13 22:33:28 +0530

    redis config

--- src/main/java/com/trackingnumber/trn/TrackingNumberApplication.java
+++ src/main/java/com/trackingnumber/trn/TrackingNumberApplication.java
@@ -1,9 +1,12 @@
 package com.trackingnumber.trn;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,8 +20,17 @@ public class TrackingNumberApplication {
 	}
 
 	@Bean
-	public LettuceConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory();
+	public JedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName("redis-10770.c244.us-east-1-2.ec2.redns.redis-cloud.com");
+		redisStandaloneConfiguration.setPort(10770);
+		redisStandaloneConfiguration.setPassword("HWReMj0qyR2guGiUu2HadB7E16zNpLg1");
+
+		GenericObjectPoolConfig<?> poolConfig = new GenericObjectPoolConfig<>();
+		JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().usePooling()
+				.poolConfig(poolConfig).build();
+
+		return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
 	}
 
 	@Bean
